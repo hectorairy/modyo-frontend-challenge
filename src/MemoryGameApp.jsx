7711be4c72eb4476685896c009d0b3c20b01bea6
Héctor Airy <hello@hectorairy.com>
2023-05-28 16:41:23 -0600

    add config to sass and tailwind

--- src/MemoryGameApp.jsx
+++ src/MemoryGameApp.jsx
@@ -1,7 +1,9 @@
+import "./styles/MemoryGameApp.scss";
+
 export const MemoryGameApp = () => {
   return (
     <>
-      <h1>MemoryGameApp</h1>
+      <h1 className="memory-game__title">Hello world!</h1>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { MemoryGameApp } from "./MemoryGameApp";
+import "./styles/global.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
