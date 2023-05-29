2cb444fbfba4a42a3f8dbbc3eb8a9b8bc4418d4b
Héctor Airy <hello@hectorairy.com>
2023-05-28 23:51:59 -0600

    refactor: avoid increase hits if click card already matched

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -3,11 +3,14 @@ import { PropTypes } from "prop-types";
 export const MemoryCard = ({ card, id, selectCard }) => {
   const { url_image, stat } = card;
   const classCard = stat ? `memory-game__card--active ${stat}` : "";
+
+  const handleClick = () => {
+    if (stat.includes("correct")) return false;
+    selectCard(id);
+  };
+
   return (
-    <div
-      className={`memory-game__card ${classCard}`}
-      onClick={() => selectCard(id)}
-    >
+    <div className={`memory-game__card ${classCard}`} onClick={handleClick}>
       <img src={url_image} alt="" />
     </div>
   );
